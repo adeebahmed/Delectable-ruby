580e90da78cba701a88fc65ee933b8921658f522
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-12-30 15:27:18 -0600

    Added validation and relations to food and menu;

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,8 @@
 class Food < ApplicationRecord
+  validates :foodname, presence: true
+  validates :category, presence: true
+  validates :calories, presence: true
+  validates :price, presence: true
+  
+  belongs_to :user
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,5 @@
 class Menu < ApplicationRecord
+  validates :menuname, presence: true
+
+  has_many :food
 end
