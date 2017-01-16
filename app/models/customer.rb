5712a8511289e14c0dca7fedfe58928258281234
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2017-01-16 13:41:31 -0600

    Added rails associations;

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :phone, presence: true
+
+  has_many :orders
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,4 @@
 class Report < ApplicationRecord
+
+  has_many :orders
 end
